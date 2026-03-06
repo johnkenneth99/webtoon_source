611d366bfbab76e3e7622a5d4071cfdfa0063792
jksinay99 <jksinay@gmail.com>
2026-03-06 16:25:07 +0800

    initial commit

---
+++ lib/webtoon_source.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "webtoon_source/version"
+
+module WebtoonSource
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/webtoon_source_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe WebtoonSource do
+  it "has a version number" do
+    expect(WebtoonSource::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
