47ba5ec2b9c5045b23cd2f71b55d6d86e354e59f
jksinay99 <jksinay@gmail.com>
2026-03-12 18:02:41 +0800

    add default storage path and `downloaded?` method

--- lib/webtoon_source.rb
+++ lib/webtoon_source.rb
@@ -6,11 +6,21 @@ require "faraday"
 
 class WebtoonSource
   class Error < StandardError; end
+
+  DEFAULT_STORAGE_PATH = File.join(Dir.home, "webtoon_source")
+
   attr_accessor :storage_path
 
   def initialize(platform = "asura_scans")
     @platform = platform
+    @storage_path = DEFAULT_STORAGE_PATH
 
     yield(self) if block_given?
+  end
+
+  # Checks if a specific chapter for a webtoon has been downloaded.
+  def downloaded?(name, chapter)
+    path = File.join(@storage_path, name, chapter.to_s)
+    Dir.exist?(path) && !Dir.empty?(path)
   end
 end
