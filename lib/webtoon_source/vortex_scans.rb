8b553e0d23b5fb3089c3769e4b8790fb08f9a79b
jksinay99 <jksinay@gmail.com>
2026-07-22 14:34:59 +0800

    refactor: rework VortexScans to use shared Panel/Chapter objects

--- lib/webtoon_source/vortex_scans.rb
+++ lib/webtoon_source/vortex_scans.rb
@@ -2,72 +2,76 @@
 
 # A source implementation for Vortex Scans.
 class WebtoonSource::VortexScans < WebtoonSource::Base
-  # The base URL for the Vortex Scans website.
   BASE_URL = "https://vortexscans.org"
-  # The base URL for the Vortex Scans CDN.
-  CDN_URL = "https://storage.vortexscans.org"
 
-  # Initializes a new instance of the Vortex Scans source.
-  #
-  # @param base_url [String] the base URL of the source.
-  # @yield [self] yields the instance to an optional block for configuration.
   def initialize(base_url = BASE_URL, &block)
     super(base_url, &block)
   end
 
-  # Downloads the panels for the current series and chapter.
-  #
-  # @return [Array<String>] the list of panel image paths.
-  # @raise [ArgumentError] if chapter_number, series_slug, or directory_name is not set.
-  def download
-    ensure_present!(:chapter_number, :series_slug, :directory_name)
+  def download(chapter_url = nil)
+    if chapter_url.nil?
+      ensure_present!(:chapter_number, :series_slug, :directory_name)
+    else
+      path_segments = chapter_url.delete_suffix("/").split("/")
 
-    path = File.join(@storage_path, @directory_name, @chapter_number.to_s)
+      @series_slug = path_segments[-2]
+      @chapter_number = path_segments[-1].split("-").last
+      @directory_name = @series_slug.gsub("-", "_")
+    end
 
-    FileUtils.mkdir_p(path) unless Dir.exist?(path)
+    chapter_directory = File.join(@storage_path, @directory_name, @chapter_number.to_s)
 
-    cdn_conn = Faraday.new(CDN_URL)
+    FileUtils.mkdir_p(chapter_directory) unless Dir.exist?(chapter_directory)
 
-    panels.each do |panel|
-      panel_path, order, file_extension = panel
-      # Format: "0001", "0002", etc.
-      panel_name = "#{order.slice(2..-1)}.#{file_extension}"
+    panel_result = panels
+    cdn_conn = Faraday.new(panel_result.base_url)
 
-      panel_storage_path = File.join(path, panel_name)
+    panel_result.panel_list.each do |panel|
+      panel_name = [panel.order.rjust(2, "0"), ".", panel.file_extension].join
+      panel_directory = File.join(chapter_directory, panel_name)
 
-      File.open(panel_storage_path, "wb") do |f|
-        cdn_conn.get(panel_path) do |response|
+      File.open(panel_directory, "wb") do |f|
+        cdn_conn.get(panel.path) do |response|
           response.options.on_data = proc { |chunk, _size| f.write chunk }
         end
       end
     end
   end
 
-  # TODO: Add support for full chapter_url as argument.
+  def panels(chapter_url = nil)
+    if chapter_url.nil?
+      ensure_present!(:series_slug, :chapter_number)
+      path = "/series/#{@series_slug}/chapter-#{@chapter_number}"
 
-  # Retrieves the list of panel image paths for the current series and chapter.
-  #
-  # @return [Array<String>] the list of panel image paths.
-  # @raise [ArgumentError] if series_slug or chapter_number is not set.
-  def panels
-    ensure_present!(:series_slug, :chapter_number)
+      response = @conn.get(path)
+    else
+      response = @conn.get(chapter_url)
+      @series_slug = chapter_url.delete_suffix("/").split("/")[-2]
+    end
 
-    path = "/series/#{@series_slug}/chapter-#{@chapter_number}"
+    doc = Nokogiri::HTML(response.body)
+    base_url = nil
 
-    response = @conn.get(path)
+    panel_list = doc.css("img[src][data-reader-index]").map do |img|
+      panel_uri = URI.parse(img["src"])
 
-    normalized_slug = @series_slug.gsub(/['"]/, "")
-    panel_pattern = %r{#{CDN_URL}(/upload/series/#{normalized_slug}.+?/page-(\d+).+?\.(webp|jpg|jpeg|png))}
+      base_url = "#{panel_uri.scheme}://#{panel_uri.host}" if base_url.nil?
+      path = panel_uri.path
 
-    response.body.scan(panel_pattern).uniq
+      Panel.new(
+        path:,
+        order: img["data-reader-index"],
+        file_extension: File.extname(path).delete_prefix(".")
+      )
+    end
+
+    PanelResult.new(
+      base_url:,
+      panel_list:
+    )
   end
 
-  # Retrieves the list of chapters for the current series.
-  #
-  # @param series_url [String, nil] an optional URL to fetch chapters from.
-  # @return [Array<Hash>] the list of chapters with metadata.
-  # @raise [ArgumentError] if series_slug is not set and no URL is provided.
-  def chapters(series_url = nil) # rubocop:disable Metrics/AbcSize
+  def chapters(series_url = nil) # rubocop:disable Metrics/AbcSize,Metrics/PerceivedComplexity
     if series_url.nil?
       ensure_present!(:series_slug)
 
@@ -81,25 +85,42 @@ class WebtoonSource::VortexScans < WebtoonSource::Base
     island = doc.at_css('astro-island[opts*="SeriesChaptersPanelIsland"]')
 
     data = JSON.parse(island["props"])
-    chapter_list = normalize(data).dig("post", "chapters")
+    normalized_data = WebtoonSource::Helpers::Transformers.normalize_astro_island_props(data)
+
+    chapter_list = normalized_data["initialChap"]
 
     return [] if chapter_list.nil?
 
     mapped_chapters = chapter_list.map do |chapter|
-      chapter_number = chapter["number"].to_s
-      chapter_path = ["/series", @series_slug, chapter_number].join("/")
-
-      metadata = chapter.except(chapter_number).transform_keys { |key| WebtoonSource::Helpers::String.snake_case(key).to_sym }
-
-      {
-        chapter_number:,
-        chapter_path:,
-        series_slug: @series_slug,
-        metadata:
+      chapter_fields = {
+        id: nil,
+        slug: nil,
+        title: nil,
+        number: nil,
+        is_locked: nil,
+        metadata: {}
       }
+
+      chapter.each do |key, value|
+        new_key = WebtoonSource::Helpers::String.snake_case(key).to_sym
+
+        if ALLOWED_CHAPTER_FIELDS.include?(key)
+          chapter_fields[new_key] = value
+        else
+          chapter_fields[:metadata][new_key] = value
+        end
+      end
+
+      chapter_fields[:number] = chapter_fields [:number].to_s
+
+      Chapter.new(
+        **chapter_fields,
+        series_slug: @series_slug,
+        path: ["/series", @series_slug, chapter_fields[:slug]].join("/")
+      )
     end
 
-    mapped_chapters.sort_by { |chapter| -chapter[:chapter_number].to_f }
+    mapped_chapters.sort_by { |chapter| -chapter.number.to_f }
   end
 
   private
